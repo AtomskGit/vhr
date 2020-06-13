b1f60ff273ee6b9ca751eb74587e427dadd82798
Atomsk <Atomsk@126.com>
2020-06-13 17:15:17 +0800

    helloworld

---
+++ vhr/vhrserver/vhr-web/src/main/java/com/atomsk/vhr/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.atomsk.vhr;
+
+/**
+ * @author Atomsk
+ * @github https://github.com/AtomskGit
+ * @blog https://atomskgit.github.io/blog/
+ * @date 2020/6/13
+ */
+public class HelloWorld {
+}
